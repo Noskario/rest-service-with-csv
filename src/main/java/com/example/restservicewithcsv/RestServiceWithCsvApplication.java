7faf220a3efc0ad1d577a35d2fb094dc4c2918d8
Oskar Nenoff <Oskar.Nenoff@web.de>
2022-08-09 21:00:12 +0200

    open website when starting SpringApplication

--- src/main/java/com/example/restservicewithcsv/RestServiceWithCsvApplication.java
+++ src/main/java/com/example/restservicewithcsv/RestServiceWithCsvApplication.java
@@ -3,11 +3,20 @@ package com.example.restservicewithcsv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class RestServiceWithCsvApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException, IOException {
 		SpringApplication.run(RestServiceWithCsvApplication.class, args);
-	}
 
+		//For convenience, I now open the started connection
+		//In order to do so, this application cannot be headless, so lets change that
+		System.setProperty("java.awt.headless", "false");
+		Desktop.getDesktop().browse(new URI("http://localhost:8899/listall"));
+	}
 }
